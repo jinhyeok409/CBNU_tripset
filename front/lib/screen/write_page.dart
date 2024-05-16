416f987579b584e90512928140d485b3cb3f94da
jinhyeok409 <74577773+jinhyeok409@users.noreply.github.com>
2024-05-16 19:25:42 +0900

    Update write_page.dart

--- front/lib/screen/write_page.dart
+++ front/lib/screen/write_page.dart
@@ -41,103 +41,109 @@ class PostPageState extends State<PostPage> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('글쓰기'),
-          centerTitle: true,
-          actions: [
-            IconButton(
-              icon: Icon(Icons.send),
+    return GestureDetector(
+      onTap: () {
+        // 포커스 해제
+        FocusScope.of(context).unfocus();
+      },
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('글쓰기'),
+            centerTitle: true,
+            actions: [
+              IconButton(
+                icon: Icon(Icons.send),
+                onPressed: () {
+                  _handleSubmit(context); // Submit 버튼을 눌렀을 때 호출되는 메서드
+                },
+              ),
+            ],
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
               onPressed: () {
-                _handleSubmit(context); // Submit 버튼을 눌렀을 때 호출되는 메서드
+                // Navigate to the PostList screen
+                print('back to Postlist');
+                //Get.to(PostList());
+                Get.back();
               },
             ),
-          ],
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () {
-              // Navigate to the PostList screen
-              print('back to Postlist');
-              //Get.to(PostList());
-              Get.back();
-            },
           ),
-        ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                TextField(
-                  controller: _titleController,
-                  decoration: InputDecoration(
-                    labelText: '제목',
-                    labelStyle: TextStyle(
-                      fontWeight: FontWeight.w300,
-                      color: Colors.blue.shade200,
-                    ),
-                    border: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.blue.shade200),
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.blue.shade200),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 16.0),
-                Row(
-                  children: [
-                    Expanded(
-                      child: RadioListTile(
-                        title: Text('자유게시판'),
-                        value: 'FREE',
-                        groupValue: _selectedCategory,
-                        onChanged: (value) {
-                          setState(() {
-                            _selectedCategory = value!;
-                            print(_selectedCategory);
-                          });
-                        },
-                        activeColor: Colors.blue.shade200,
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  TextField(
+                    controller: _titleController,
+                    decoration: InputDecoration(
+                      labelText: '제목',
+                      labelStyle: TextStyle(
+                        fontWeight: FontWeight.w300,
+                        color: Colors.blue.shade200,
+                      ),
+                      border: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue.shade200),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue.shade200),
                       ),
                     ),
-                    Expanded(
-                      child: RadioListTile(
-                        title: Text('계획게시판'),
-                        value: 'PLAN',
-                        groupValue: _selectedCategory,
-                        onChanged: (value) {
-                          setState(() {
-                            _selectedCategory = value!;
-                            print(_selectedCategory);
-                          });
-                        },
-                        activeColor: Colors.blue.shade200,
+                  ),
+                  SizedBox(height: 16.0),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: RadioListTile(
+                          title: Text('자유게시판'),
+                          value: 'FREE',
+                          groupValue: _selectedCategory,
+                          onChanged: (value) {
+                            setState(() {
+                              _selectedCategory = value!;
+                              print(_selectedCategory);
+                            });
+                          },
+                          activeColor: Colors.blue.shade200,
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 16.0),
-                TextField(
-                  controller: _contentController,
-                  style: TextStyle(
-                    fontSize: 18,
+                      Expanded(
+                        child: RadioListTile(
+                          title: Text('계획게시판'),
+                          value: 'PLAN',
+                          groupValue: _selectedCategory,
+                          onChanged: (value) {
+                            setState(() {
+                              _selectedCategory = value!;
+                              print(_selectedCategory);
+                            });
+                          },
+                          activeColor: Colors.blue.shade200,
+                        ),
+                      ),
+                    ],
                   ),
-                  decoration: InputDecoration(
-                    labelText: '내용을 입력하세요.',
-                    labelStyle: TextStyle(
-                      fontWeight: FontWeight.w300,
-                      color: Colors.blue.shade200,
+                  SizedBox(height: 16.0),
+                  TextField(
+                    controller: _contentController,
+                    style: TextStyle(
+                      fontSize: 18,
                     ),
-                    border: InputBorder.none,
+                    decoration: InputDecoration(
+                      labelText: '내용을 입력하세요.',
+                      labelStyle: TextStyle(
+                        fontWeight: FontWeight.w300,
+                        color: Colors.blue.shade200,
+                      ),
+                      border: InputBorder.none,
+                    ),
+                    maxLines: null, // 텍스트 필드의 높이를 사용자가 입력한 텍스트에 따라 자동으로 조정
                   ),
-                  maxLines: null, // 텍스트 필드의 높이를 사용자가 입력한 텍스트에 따라 자동으로 조정
-                ),
-                SizedBox(height: 16.0),
-              ],
+                  SizedBox(height: 16.0),
+                ],
+              ),
             ),
           ),
         ),
