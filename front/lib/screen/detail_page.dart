07efbeb2f44dc41a7033bc1caeb5665b27590f70
jinhyeok409 <74577773+jinhyeok409@users.noreply.github.com>
2024-05-17 16:51:20 +0900

    Update detail_page.dart

--- front/lib/screen/detail_page.dart
+++ front/lib/screen/detail_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:front/screen/login.dart';
+import 'package:front/screen/post_list.dart';
 import 'package:http/http.dart' as http;
 import 'package:front/screen/detail_page.dart';
 import 'package:get/get.dart';
@@ -96,7 +97,7 @@ class PostDetailPageState extends State<PostDetailPage> {
         // 삭제 성공 시
         print("게시물 삭제 성공");
         // 이전 화면으로 이동 또는 다른 작업 수행
-        Get.back();
+        Get.off(PostList());
       } else {
         print(postId);
         print(response.statusCode);
@@ -122,7 +123,7 @@ class PostDetailPageState extends State<PostDetailPage> {
             // 왼쪽에 뒤로가기 아이콘 추가
             icon: Icon(Icons.arrow_back),
             onPressed: () {
-              Get.back();
+              Get.off(PostList());
               // GET.off 써서 뒤로가기
             },
           ),
@@ -131,15 +132,18 @@ class PostDetailPageState extends State<PostDetailPage> {
               IconButton(
                 icon: Icon(Icons.edit),
                 onPressed: () {
+                  String postId = Get.arguments;
+                  print(postId);
                   // 게시물 수정 페이지로 이동
-                  Get.to(() => EditPostPage(title: title, content: content));
+                  Get.to(() => EditPostPage(
+                      title: title, content: content, postId: postId));
                 },
               ),
             if (isAuthorVerified(username, tokenUsername)) // 저자 확인 함수 호출
               IconButton(
                 icon: Icon(Icons.delete),
                 onPressed: () {
-                  String postId = "your_post_id_here"; // 삭제할 게시물의 ID
+                  String postId = Get.arguments;
                   deletePost(postId);
                 },
               ),
@@ -194,13 +198,86 @@ class PostDetailPageState extends State<PostDetailPage> {
   }
 }
 
-class EditPostPage extends StatelessWidget {
-  final TextEditingController _titleController = TextEditingController();
-  final TextEditingController _contentController = TextEditingController();
+class EditPostPage extends StatefulWidget {
   final String title; // 원래 제목
-  final String content; // 원래 이름
+  final String content; // 원래 내용
+  final String postId; // id
 
-  EditPostPage({super.key, required this.title, required this.content});
+  EditPostPage({
+    Key? key,
+    required this.title,
+    required this.content,
+    required this.postId,
+  }) : super(key: key);
+
+  @override
+  _EditPostPageState createState() => _EditPostPageState();
+}
+
+class _EditPostPageState extends State<EditPostPage> {
+  late TextEditingController _posttitleController;
+  late TextEditingController _postcontentController;
+  String _selectedCategory = 'FREE';
+
+  @override
+  void initState() {
+    super.initState();
+    _posttitleController = TextEditingController(text: widget.title);
+    _postcontentController = TextEditingController(text: widget.content);
+  }
+
+  @override
+  void dispose() {
+    _posttitleController.dispose();
+    _postcontentController.dispose();
+    super.dispose();
+  }
+
+  void updatePost(BuildContext context) async {
+    print("포스트 아이디");
+    print(widget.postId);
+    final storage = FlutterSecureStorage();
+    String serverUri = dotenv.env['SERVER_URI']!;
+    String postUpdateEndpoint =
+        dotenv.env['POST_UPDATE_ENDPOINT']!; // 업데이트할 게시물의 URL
+    String updateUrl = "$serverUri$postUpdateEndpoint/${widget.postId}";
+    String category = _selectedCategory;
+
+    String? token = await storage.read(key: 'accessToken');
+    Map<String, String> headers = {
+      'Authorization': '$token', // 토큰 값 추가
+    };
+
+    String newTitle = _posttitleController.text;
+    String newContent = _postcontentController.text;
+
+    try {
+      final response = await http.put(
+        Uri.parse(updateUrl),
+        headers: headers,
+        body: {
+          'title': newTitle,
+          'content': newContent,
+          'category': category,
+        },
+      );
+
+      if (response.statusCode == 200) {
+        // 게시물 업데이트가 성공한 경우
+        print('게시물이 성공적으로 업데이트되었습니다.');
+        Get.off(PostDetailPage(), arguments: widget.postId.toString());
+      } else {
+        // 게시물 업데이트가 실패한 경우
+        print('게시물 업데이트 실패: ${response.statusCode}');
+        // 실패 메시지를 사용자에게 보여줄 수 있습니다.
+        // 예를 들어 Get 패키지를 사용하여 에러 다이얼로그를 표시할 수 있습니다.
+      }
+    } catch (e) {
+      // 예외 발생 시 처리
+      print('게시물 업데이트 중 에러 발생: $e');
+      // 예외 처리 로직을 추가할 수 있습니다.
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -234,8 +311,7 @@ class EditPostPage extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               TextFormField(
-                controller: _titleController,
-                initialValue: title,
+                controller: _posttitleController,
                 decoration: InputDecoration(
                   labelText: '제목',
                   labelStyle: TextStyle(
@@ -251,9 +327,41 @@ class EditPostPage extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 16),
+              Row(
+                children: [
+                  Expanded(
+                    child: RadioListTile(
+                      title: Text('자유게시판'),
+                      value: 'FREE',
+                      groupValue: _selectedCategory,
+                      onChanged: (value) {
+                        setState(() {
+                          _selectedCategory = value!;
+                          print(_selectedCategory);
+                        });
+                      },
+                      activeColor: Colors.blue.shade200,
+                    ),
+                  ),
+                  Expanded(
+                    child: RadioListTile(
+                      title: Text('계획게시판'),
+                      value: 'PLAN',
+                      groupValue: _selectedCategory,
+                      onChanged: (value) {
+                        setState(() {
+                          _selectedCategory = value!;
+                          print(_selectedCategory);
+                        });
+                      },
+                      activeColor: Colors.blue.shade200,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16.0),
               TextFormField(
-                controller: _contentController,
-                initialValue: content,
+                controller: _postcontentController,
                 maxLines: null,
                 keyboardType: TextInputType.multiline,
                 decoration: InputDecoration(
@@ -262,12 +370,7 @@ class EditPostPage extends StatelessWidget {
                     fontWeight: FontWeight.w300,
                     color: Colors.blue.shade200,
                   ),
-                  border: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.blue.shade200),
-                  ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.blue.shade200),
-                  ),
+                  border: InputBorder.none,
                 ),
               ),
               SizedBox(height: 16),
@@ -276,36 +379,5 @@ class EditPostPage extends StatelessWidget {
         ),
       ),
     );
-  }
-
-  void updatePost(BuildContext context) async {
-    String updateUrl = 'http://13.124.239.15/update/'; // 업데이트할 게시물의 URL
-    String newTitle = _titleController.text;
-    String newContent = _contentController.text;
-    try {
-      final response = await http.put(
-        Uri.parse(updateUrl),
-        body: {
-          'title': newTitle,
-          'content': newContent,
-        },
-      );
-
-      if (response.statusCode == 200) {
-        // 게시물 업데이트가 성공한 경우
-        print('게시물이 성공적으로 업데이트되었습니다.');
-        // 업데이트가 완료되면 이전 화면으로 이동
-        Navigator.of(context).pop();
-      } else {
-        // 게시물 업데이트가 실패한 경우
-        print('게시물 업데이트 실패: ${response.statusCode}');
-        // 실패 메시지를 사용자에게 보여줄 수 있습니다.
-        // 예를 들어 Get 패키지를 사용하여 에러 다이얼로그를 표시할 수 있습니다.
-      }
-    } catch (e) {
-      // 예외 발생 시 처리
-      print('게시물 업데이트 중 에러 발생: $e');
-      // 예외 처리 로직을 추가할 수 있습니다.
-    }
   }
 }
